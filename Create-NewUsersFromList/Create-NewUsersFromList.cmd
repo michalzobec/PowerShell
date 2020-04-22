7180b62a39ea6191cc353ef24826249997cbecd9
Michal Zobec <michal@zobec.net>
2020-04-22 15:03:08 +0200

    Update Create-NewUsersFromList.cmd
    Small update

--- Create-NewUsersFromList/Create-NewUsersFromList.cmd
+++ Create-NewUsersFromList/Create-NewUsersFromList.cmd
@@ -1,4 +1,5 @@
 @echo off
+rem launcher for classic powershell (powershell from v1 to v5)
 powershell.exe -ExecutionPolicy Bypass -File "%~d0%~p0%~n0.ps1"
 If not "%errorlevel%"=="0" (
 Echo command failed
